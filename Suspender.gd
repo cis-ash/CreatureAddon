fad649f3821e20e1d196fabda0dbab3dc318fc2c
ash_kmn <kamensky@sheridancollege.ca>
2021-10-25 15:46:37 -0400

    add support

--- Suspender.gd
+++ Suspender.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends KinematicBody2D
 
 func _process(delta):
 	if Input.is_action_pressed("ui_accept"):
 		var to_mouse = get_global_mouse_position() - global_position
-		position += to_mouse * 10 * delta
+		move_and_slide(to_mouse * 10)
 		pass
 	pass
